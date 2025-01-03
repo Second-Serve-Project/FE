4a5a84c52fd9afbba5204c4b126f1dd03934d0d6
Kwon <kdg5436@gmail.com>
2025-01-03 18:04:21 +0900

    ✨feat: 마이페이지 스타일, 가게리스트 추가

--- src/components/MemberInfo/memberinfo.js
+++ src/components/MemberInfo/memberinfo.js
@@ -1,4 +1,13 @@
 import React from "react";
+import point from "../../images/point.svg";
+import logout from "../../images/logout.svg";
+import pwchange from "../../images/pwchange.svg";
+import noti from "../../images/noti.svg";
+
+//가게 이미지도 나중에 백에서 불러와야 함
+import store from "../../images/store.png";
+
+import star from "../../images/star.svg";
 
 function MemberInfo() {
   return (
@@ -8,20 +17,62 @@ function MemberInfo() {
           <p>qwerty123님</p>
         </div>
         <div className="member_point">
-          <p>5,100 포인트</p>
+          <p>
+            <img id="point" src={point} alt="Point"></img> 5,100 포인트
+          </p>
         </div>
       </div>
       <div className="btn">
         <div className="pw_change">
           <button className="pw_changebtn" type="submit">
-            비밀번호 변경
+            <img id="pwchange" src={pwchange} alt="Pwchange"></img> 비밀번호
+            변경
           </button>
         </div>
         <div className="logout">
           <button className="logoutbtn" type="submit">
-            로그아웃
+            <img id="logout" src={logout} alt="Logout"></img> 로그아웃
           </button>
         </div>
+      </div>
+      <div className="usedStore">
+        <div className="useWrite">
+          <p>이용했던 가게</p>
+        </div>
+
+        <button className="storeButton">
+          <div className="oneStore">
+            <img src={store} className="storeImg" />
+            <div className="storeName">
+              <a>가게 이름</a>
+            </div>
+            <div className="storeInfo">
+              <img src={star} />
+              <a>0.0</a>
+              <a>·</a>
+              <a>000m</a>
+            </div>
+          </div>
+        </button>
+
+        <button className="storeButton">
+          <div className="oneStore">
+            <img src={store} className="storeImg" />
+            <div className="storeName">
+              <a>가게 이름</a>
+            </div>
+            <div className="storeInfo">
+              <img src={star} />
+              <a>0.0</a>
+              <a>·</a>
+              <a>000m</a>
+            </div>
+          </div>
+        </button>
+
+        {/*
+          가게들 추가로 불러오기
+           */}
       </div>
     </div>
   );
